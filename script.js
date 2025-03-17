ae6e8da86e458f820ba28b4f94bac3bdbf735e02
Ritika <ritikapriya376@gmail.com>
2025-03-17 22:41:35 +0530

    time added

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function addTask(){
     const inputTask = document.getElementById('inputTask').value.trim();
+    const timeValue1 = document.getElementById('timePicker1').value;
+    const timeValue2 = document.getElementById('timePicker2').value;
     if (inputTask === "") {
         alert("Please enter a task!"); 
         document.getElementById('inputTask').value=""
@@ -7,8 +9,9 @@ function addTask(){
     }
     const newTask = document.createElement('li')
     const taskList = document.getElementById('taskList')
+    newTask.textContent = `Started at: ${timeValue1} ${inputTask} Due at: ${timeValue2}`
     taskList.appendChild(newTask)
-    newTask.textContent = inputTask
+    // newTask.textContent = inputTask
     // newTask.textContent = document.getElementById('inputTask').value
     document.getElementById('inputTask').value=""
     deleteTask(newTask)
